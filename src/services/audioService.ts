1d0be9c6279bb91a86e9ef4b211492e897e63820
David Pricco <dpricco@gmail.com>
2026-09-09 23:37:13 -0700

    Force continuous transcription on Android

--- src/services/audioService.ts
+++ src/services/audioService.ts
@@ -2,6 +2,7 @@ import * as idb from 'idb-keyval';
 
 export class AudioService {
   private recognition: any = null;
+  private isIntentionallyStopped: boolean = false;
   
   public onTranscriptUpdate: (text: string, isFinal: boolean) => void = () => {};
 
@@ -19,7 +20,7 @@ export class AudioService {
         
         for (let i = event.resultIndex; i < event.results.length; ++i) {
           if (event.results[i].isFinal) {
-            finalTranscript += event.results[i][0].transcript;
+            finalTranscript += event.results[i][0].transcript + ' ';
           } else {
             interimTranscript += event.results[i][0].transcript;
           }
@@ -35,6 +36,20 @@ export class AudioService {
 
       this.recognition.onerror = (event: any) => {
         console.error('Speech recognition error', event.error);
+        if (event.error === 'not-allowed' || event.error === 'service-not-allowed') {
+          this.isIntentionallyStopped = true;
+        }
+      };
+
+      this.recognition.onend = () => {
+        // If the browser stopped it automatically (due to pause), restart it!
+        if (!this.isIntentionallyStopped && this.recognition) {
+          try {
+            this.recognition.start();
+          } catch (e) {
+            console.error('Failed to restart recognition', e);
+          }
+        }
       };
     } else {
       console.warn('Web Speech API not supported in this browser.');
@@ -42,6 +57,7 @@ export class AudioService {
   }
 
   async startRecording() {
+    this.isIntentionallyStopped = false;
     try {
       if (this.recognition) {
         this.recognition.start();
@@ -52,6 +68,7 @@ export class AudioService {
   }
 
   stopRecording(): Promise<Blob | null> {
+    this.isIntentionallyStopped = true;
     return new Promise((resolve) => {
       if (this.recognition) {
         this.recognition.stop();
